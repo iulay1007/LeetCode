ea28db03c54326e76554750dbfe05ba2c12cefbc
sy <905626971@qq.com>
2020-08-15 19:08:00 +0800

    leetcode

---
+++ Solution700.java
@@ -0,0 +1,17 @@
+/*700. 二叉搜索树中的搜索
+给定二叉搜索树（BST）的根节点和一个值。 你需要在BST中找到节点值等于给定值的节点。
+ 返回以该节点为根的子树。 如果节点不存在，则返回 NULL。
+*/
+class Solution700 {
+     public TreeNode searchBST(TreeNode root, int val) {
+        if (root == null )
+            return null;
+
+        if (root.val > val) {
+            return searchBST(root.left, val);
+        } else if (root.val < val) {
+            return searchBST(root.right, val);
+        } 
+        return root;
+    }
+}
