dd922418576b985c19cd1d2be5d6422ebe6ae132
sy <905626971@qq.com>
2020-09-29 21:08:55 +0800

    leetcode++

---
+++ Solution938.java
@@ -0,0 +1,47 @@
+/**
+ * 938. 二叉搜索树的范围和
+ * 给定二叉搜索树的根结点 root，返回 L 和 R（含）之间的所有结点的值的和。
+ *
+ * 二叉搜索树保证具有唯一的值。
+ *
+ * 
+ *
+ * 示例 1：
+ *
+ * 输入：root = [10,5,15,3,7,null,18], L = 7, R = 15
+ * 输出：32
+ * 示例
+ *
+ * 输入：root = [10,5,15,3,7,13,18,1,null,6], L = 6, R = 10
+ * 输出：23
+ *
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution938 {
+
+
+
+
+
+        int result;
+        int temp;
+        public int rangeSumBST(TreeNode root, int L, int R) {
+            if (root == null) return 0;
+            if (root.val >= L && root.val <= R)
+                result+=root.val;
+
+
+            temp += rangeSumBST(root.left, L, R);
+            temp += rangeSumBST(root.right, L, R);
+
+
+            return result;
+
+        }
+    }
