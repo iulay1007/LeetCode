ab6738213525985e5ad4073e1e24c477915fb784
sy <905626971@qq.com>
2020-11-03 23:42:27 +0800

    leetcode++

---
+++ Solution430.java
@@ -0,0 +1,41 @@
+
+/*
+430. 扁平化多级双向链表
+多级双向链表中，除了指向下一个节点和前一个节点指针之外，它还有一个子链表指针，可能指向单独的双向链表。这些子列表也可能会有一个或多个自己的子项，依此类推，生成多级数据结构，如下面的示例所示。
+
+给你位于列表第一级的头节点，请你扁平化列表，使所有结点出现在单级双链表中。
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node prev;
+    public Node next;
+    public Node child;
+};
+*/
+
+class Solution430{
+    Node prv=null;
+    public Node flatten(Node head){
+        if (head==null)
+            return head;
+        dfs(head);
+        return head;
+
+    }
+
+    private void dfs(Node node) {
+        if(node==null)
+            return;
+        Node next=node.next;
+        if(prv!=null)
+        {
+            prv.next=node;
+            node.prev=prv;
+        }
+        prv=node;
+        dfs(node.child);
+        node.child=null;
+        dfs(next);
+
+    }
+}
