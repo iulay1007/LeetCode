004402cb6417706becf3c25246839e9b69bdde53
sy <905626971@qq.com>
2020-11-06 00:18:02 +0800

    leetcode++

--- Solution24.java
+++ Solution24.java
@@ -1,4 +1,22 @@
 /**
+ * 24. 两两交换链表中的节点
+ * 给定一个链表，两两交换其中相邻的节点，并返回交换后的链表。
+ *
+ * 你不能只是单纯的改变节点内部的值，而是需要实际的进行节点交换。
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：head = [1,2,3,4]
+ * 输出：[2,1,4,3]
+ * 示例 2：
+ *
+ * 输入：head = []
+ * 输出：[]
+ * 示例 3：
+ *
+ * 输入：head = [1]
+ * 输出：[1]
  * Definition for singly-linked list.
  * public class ListNode {
  *     int val;
@@ -8,23 +26,83 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+
+//我也不知道为什么对了555
 class Solution24 {
-    ListNode prv;
-    public ListNode swapPairs(ListNode head) {
-        if(head==null||head.next==null)
+
+        ListNode prv;
+        public ListNode swapPairs(ListNode head) {
+            if(head==null||head.next==null)
+                return head;
+            prv=head;
+            head=head.next;
+            change(prv);
             return head;
-        prv=head;
-        head=head.next;
-        change(prv);
-        return head;
+        }
+
+        private ListNode change(ListNode node) {
+            if(node==null||node.next==null)
+                return node;
+            ListNode temp=node.next;
+            node.next=change(temp.next);
+            temp.next=node;
+            return temp;
+        }
     }
 
-    private void change(ListNode node) {
-        if(node==null||node.next==null)
-            return;
-        ListNode temp=node.next;
-        temp.next=node;
-        change(temp);
-
+    /*
+    //简单的递归
+    class Solution {
+    public ListNode swapPairs(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode newHead = head.next;
+        head.next = swapPairs(newHead.next);
+        newHead.next = head;
+        return newHead;
     }
 }
+
+*/
+
+
+//在不确定某段的头结点时，用哑结点非常方便！！！！
+/*迭代
+思路与算法
+
+也可以通过迭代的方式实现两两交换链表中的节点。
+
+创建哑结点 dummyHead，令 dummyHead.next = head。令 temp 表示当前到达的节点，初始时 temp = dummyHead。每次需要交换 temp 后面的两个节点。
+
+如果 temp 的后面没有节点或者只有一个节点，则没有更多的节点需要交换，因此结束交换。否则，获得 temp 后面的两个节点 node1 和 node2，通过更新节点的指针关系实现两两交换节点。
+
+具体而言，交换之前的节点关系是 temp -> node1 -> node2，交换之后的节点关系要变成 temp -> node2 -> node1，因此需要进行如下操作。
+
+
+temp.next = node2
+node1.next = node2.next
+node2.next = node1
+完成上述操作之后，节点关系即变成 temp -> node2 -> node1。再令 temp = node1，对链表中的其余节点进行两两交换，直到全部节点都被两两交换。
+
+两两交换链表中的节点之后，新的链表的头节点是 dummyHead.next，返回新的链表的头节点即可
+class Solution {
+    public ListNode swapPairs(ListNode head) {
+        ListNode dummyHead = new ListNode(0);
+        dummyHead.next = head;
+        ListNode temp = dummyHead;
+        while (temp.next != null && temp.next.next != null) {
+            ListNode node1 = temp.next;
+            ListNode node2 = temp.next.next;
+            temp.next = node2;
+            node1.next = node2.next;
+            node2.next = node1;
+            temp = node1;
+        }
+        return dummyHead.next;
+    }
+}
+
+链接：https://leetcode-cn.com/problems/swap-nodes-in-pairs/solution/liang-liang-jiao-huan-lian-biao-zhong-de-jie-di-91/
+
+*/
