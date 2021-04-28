06b5078e0160e400aaf6c197e66aff9530fff825
sy <905626971@qq.com>
2021-04-28 23:52:21 +0800

    leetcode++

---
+++ Solution108.java
@@ -0,0 +1,44 @@
+
+/*
+给你一个整数数组 nums ，其中元素已经按 升序 排列，请你将其转换为一棵 高度平衡 二叉搜索树。
+
+高度平衡 二叉树是一棵满足「每个节点的左右两个子树的高度差的绝对值不超过 1 」的二叉树。
+
+ 
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/convert-sorted-array-to-binary-search-tree
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+输入：nums = [-10,-3,0,5,9]
+输出：[0,-3,9,-10,null,5]
+解释：[0,-10,5,null,-3,null,9] */
+
+/*
+[0,1,2,3,4,5]
+输出：
+[3,2,4,1,null,null,5,0]
+预期结果：
+[3,1,5,0,2,4]
+
+[0,1,2,3,4,5,6,7,8,9]
+[5,2,8,1,4,7,9,0,null,3,null,6]
+*/
+public class Solution108 {
+    public TreeNode sortedArrayToBST(int[] nums) {
+        return helper(nums, 0, nums.length - 1);
+    }
+
+    public TreeNode helper(int[] nums, int left, int right) {
+        if (left > right) {
+            return null;
+        }
+
+        int mid = (left + right) / 2;
+
+        TreeNode root = new TreeNode(nums[mid]);
+        root.left = helper(nums, left, mid - 1);
+        root.right = helper(nums, mid + 1, right);
+        return root;
+    }
+
+}
