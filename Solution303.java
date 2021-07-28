075803c7b2e0b76cd5f31bbc5545762b65a45fdf
sy <905626971@qq.com>
2021-07-28 16:00:21 +0800

    leetcode++

---
+++ Solution303.java
@@ -0,0 +1,82 @@
+/*
+* 303. 区域和检索 - 数组不可变
+给定一个整数数组  nums，求出数组从索引 i 到 j（i ≤ j）范围内元素的总和，包含 i、j 两点。
+
+实现 NumArray 类：
+
+NumArray(int[] nums) 使用数组 nums 初始化对象
+int sumRange(int i, int j) 返回数组 nums 从索引 i 到 j（i ≤ j）范围内元素的总和，包含 i、j 两点（也就是 sum(nums[i], nums[i + 1], ... , nums[j])）
+
+
+示例：
+
+输入：
+["NumArray", "sumRange", "sumRange", "sumRange"]
+[[[-2, 0, 3, -5, 2, -1]], [0, 2], [2, 5], [0, 5]]
+输出：
+[null, 1, -1, -3]
+
+解释：
+NumArray numArray = new NumArray([-2, 0, 3, -5, 2, -1]);
+numArray.sumRange(0, 2); // return 1 ((-2) + 0 + 3)
+numArray.sumRange(2, 5); // return -1 (3 + (-5) + 2 + (-1))
+numArray.sumRange(0, 5); // return -3 ((-2) + 0 + 3 + (-5) + 2 + (-1))
+
+
+提示：
+
+0 <= nums.length <= 104
+-105 <= nums[i] <= 105
+0 <= i <= j < nums.length
+最多调用 104 次 sumRange 方法
+* */
+
+public class Solution303 {
+    class NumArray {
+        private int []nums;
+
+        public NumArray(int[] nums) {
+
+        for(int i = 1 ; i< nums.length ;i++){
+           nums[i]=nums[i-1]+nums[i];
+        }
+        this.nums = nums;
+        }
+
+        public int sumRange(int left, int right) {
+            if(left == 0 )
+                return nums[right];
+            else return nums[right]-nums[left-1];
+        }
+    }
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray obj = new NumArray(nums);
+ * int param_1 = obj.sumRange(left,right);
+ */
+}
+/*
+官方题解
+将前缀和数组 sums 的长度设为 n+1的目的是为了方便计算 sumRange(i,j)，不需要对 i=0的情况特殊处理。此时有：
+
+sumRange(i,j)=sums[j+1]−sums[i]
+
+
+* class NumArray {
+    int[] sums;
+
+    public NumArray(int[] nums) {
+        int n = nums.length;
+        sums = new int[n + 1];
+        for (int i = 0; i < n; i++) {
+            sums[i + 1] = sums[i] + nums[i];
+        }
+    }
+
+    public int sumRange(int i, int j) {
+        return sums[j + 1] - sums[i];
+    }
+}
+。*/
+
