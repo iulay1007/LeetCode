be8b072ecc1e511bcf295be43dacf33fd9ace2b7
sy <905626971@qq.com>
2021-12-16 09:32:53 +0800

    leetcode++

---
+++ OfferII89.java
@@ -0,0 +1,41 @@
+package 剑指offer;
+
+/**
+ * 剑指 Offer II 089. 房屋偷盗
+ * 一个专业的小偷，计划偷窃沿街的房屋。每间房内都藏有一定的现金，影响小偷偷窃的唯一制约因素就是相邻的房屋装有相互连通的防盗系统，如果两间相邻的房屋在同一晚上被小偷闯入，系统会自动报警。
+ * <p>
+ * 给定一个代表每个房屋存放金额的非负整数数组 nums ，请计算 不触动警报装置的情况下 ，一夜之内能够偷窃到的最高金额。
+ * <p>
+ * <p>
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：nums = [1,2,3,1]
+ * 输出：4
+ * 解释：偷窃 1 号房屋 (金额 = 1) ，然后偷窃 3 号房屋 (金额 = 3)。
+ * 偷窃到的最高金额 = 1 + 3 = 4 。
+ * 示例 2：
+ * <p>
+ * 输入：nums = [2,7,9,3,1]
+ * 输出：12
+ * 解释：偷窃 1 号房屋 (金额 = 2), 偷窃 3 号房屋 (金额 = 9)，接着偷窃 5 号房屋 (金额 = 1)。
+ * 偷窃到的最高金额 = 2 + 9 + 1 = 12 。
+ */
+public class OfferII89 {
+    public int rob(int[] nums) {
+        if(nums.length==1)
+            return nums[0];
+        else if(nums.length ==2){
+            return Math.max(nums[0],nums[1]);
+        }
+
+        int[] dp = new int[nums.length];
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0],nums[1]);
+        for(int i =2;i<nums.length;i++){
+            dp[i] = Math.max(nums[i]+dp[i-2],dp[i-1]);
+        }
+        return dp[nums.length-1];
+    }
+
+}
