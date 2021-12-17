f029293b58f53df3e6ce4d9fb61699eb86313c7e
sy <905626971@qq.com>
2021-12-17 09:14:52 +0800

    leetcode++

---
+++ OfferII91.java
@@ -0,0 +1,65 @@
+package 剑指offer;
+
+import java.util.Arrays;
+
+/**
+ * 剑指 Offer II 091. 粉刷房子
+ * 假如有一排房子，共 n 个，每个房子可以被粉刷成红色、蓝色或者绿色这三种颜色中的一种，你需要粉刷所有的房子并且使其相邻的两个房子颜色不能相同。
+ * <p>
+ * 当然，因为市场上不同颜色油漆的价格不同，所以房子粉刷成不同颜色的花费成本也是不同的。每个房子粉刷成不同颜色的花费是以一个 n x 3 的正整数矩阵 costs 来表示的。
+ * <p>
+ * 例如，costs[0][0] 表示第 0 号房子粉刷成红色的成本花费；costs[1][2] 表示第 1 号房子粉刷成绿色的花费，以此类推。
+ * <p>
+ * 请计算出粉刷完所有房子最少的花费成本。
+ * <p>
+ * <p>
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入: costs = [[17,2,17],[16,16,5],[14,3,19]]
+ * 输出: 10
+ * 解释: 将 0 号房子粉刷成蓝色，1 号房子粉刷成绿色，2 号房子粉刷成蓝色。
+ * 最少花费: 2 + 5 + 3 = 10。
+ * 示例 2：
+ * <p>
+ * 输入: costs = [[7,6,2]]
+ * 输出: 2
+ * <p>
+ * <p>
+ * 提示:
+ * <p>
+ * costs.length == n
+ * costs[i].length == 3
+ * 1 <= n <= 100
+ * 1 <= costs[i][j] <= 20
+ */
+public class OfferII91 {
+    public int minCost(int[][] costs) {
+        int[][] dp = new int[costs.length][3];
+        dp[0] = costs[0];
+        for (int i = 1; i < costs.length; i++) {
+            dp[i][0] = Math.min(dp[i - 1][1], dp[i - 1][2]) + costs[i][0];
+            dp[i][1] = Math.min(dp[i - 1][0], dp[i - 1][2]) + costs[i][1];
+            dp[i][2] = Math.min(dp[i - 1][0], dp[i - 1][1]) + costs[i][2];
+        }
+        return Math.min(Math.min(dp[costs.length - 1][0], dp[costs.length - 1][1])
+                , dp[costs.length - 1][2]);
+
+    }
+
+    public int minCost2(int[][] costs) {
+        int num1 = costs[0][0];
+        int num2 = costs[0][1];
+        int num3 = costs[0][2];
+        int temp1, temp2, temp3;
+        for (int i = 1; i < costs.length; i++) {
+            temp1 = Math.min(num2, num3) + costs[i][0];
+            temp2 = Math.min(num1, num3) + costs[i][1];
+            temp3 = Math.min(num1, num2) + costs[i][2];
+            num1 = temp1;
+            num2 = temp2;
+            num3 = temp3;
+        }
+        return Math.min(Math.min(num1, num2), num3);
+    }
+}
