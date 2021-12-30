bdd55ebf5c0be2d9c592a653fe7f7ec8be501aa1
sy <905626971@qq.com>
2021-12-30 22:29:31 +0800

    leetcode++

---
+++ Solution55.java
@@ -0,0 +1,38 @@
+package 剑指offer;
+/**55. 跳跃游戏
+ 给定一个非负整数数组 nums ，你最初位于数组的 第一个下标 。
+
+ 数组中的每个元素代表你在该位置可以跳跃的最大长度。
+
+ 判断你是否能够到达最后一个下标。
+
+
+
+ 示例 1：
+
+ 输入：nums = [2,3,1,1,4]
+ 输出：true
+ 解释：可以先跳 1 步，从下标 0 到达下标 1, 然后再从下标 1 跳 3 步到达最后一个下标。
+ 示例 2：
+
+ 输入：nums = [3,2,1,0,4]
+ 输出：false
+ 解释：无论怎样，总会到达下标为 3 的位置。但该下标的最大跳跃长度是 0 ， 所以永远不可能到达最后一个下标。
+ */
+public class Solution55 {
+    //题解yyds
+    public boolean canJump(int[] nums) {
+        int n = nums.length;
+        int farthest = 0;
+        for (int i = 0; i < n - 1; i++) {
+            farthest = Math.max(farthest, i + nums[i]);
+            //最远跳到值为0的地方，为0的地方不能再跳了
+            if (farthest == i) {
+                return false;
+            }
+        }
+        return farthest >= n - 1;
+    }
+
+    }
+}
